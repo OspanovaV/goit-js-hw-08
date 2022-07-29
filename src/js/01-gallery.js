6cf65145819a2ae53c447704c229d371c597aca7
OspanovaVita <vitaospanova@ukr.net>
2022-07-29 22:02:37 +0300

    Исправляет ошибки

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -10,19 +10,15 @@
 // import "simplelightbox/dist/simple-lightbox.min.css";
 
 import { galleryItems } from './gallery-items';
-
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
 const galleryContainer = document.querySelector('.gallery');
 const cardsMarcup = createGalleryCardsMarkup(galleryItems);
-
 //рэндерим разметку в html 
 galleryContainer.insertAdjacentHTML('beforeend', cardsMarcup);
-
 //Реализация делегирования на div.gallery
 galleryContainer.addEventListener('click', onGalleryContainerClick);
-
 //создаём разметку
 function createGalleryCardsMarkup(galleryItems) {
     return galleryItems.map(({ preview, original, description }) => {
@@ -40,26 +36,22 @@ function createGalleryCardsMarkup(galleryItems) {
     })
     .join('');
 }
+//используем библиотеку simplelightbox для открытия модалки
+let gallery = new SimpleLightbox('.gallery a');
 
 function onGalleryContainerClick(evt) {
     evt.preventDefault();
-    const isGalleryImage = evt.target.classList.contains('gallery__image');
-  
+    const isGalleryImage = evt.target.classList.contains('gallery__image');  
     if (!isGalleryImage) {
         return;
     }
-    const urlEl = evt.target.dataset.source; //получение url большого изображения
-   
-//используем библиотеку simplelightbox для открытия модалки
-    let gallery = new SimpleLightbox('.gallery a');
-gallery.on('show.simplelightbox', function () {
-	
-});   
+//получение url большого изображения
+    const urlEl = evt.target.dataset.source;       
    //закроем модалку при нажатии клавиши ESC   
 function onEscKeyPress(event) {
   const ESC_KEY_CODE = 'Escape'; 
-  if (event.code === ESC_KEY_CODE) { //если значение клавиши= ESC
-//закрыть модалку при помощи метода библиотеки
+  if (event.code === ESC_KEY_CODE) { 
+//закрыть модалку 
   instance.close(() => window.removeEventListener('keydown', onEscKeyPress));
        return;
   }
